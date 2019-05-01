bccac460b5950b4c6bc48fe2102a01d54967f649
turbomartine <petlundbreiby@gmail.com>
2019-05-01 10:14:02 +0300

    make new home page, stored in contact at the moment, will be moved to home-page

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -1,16 +1,77 @@
 <template>
   <section class="contact-us">
-    <p>Hello</p>
+    <NavCard header="About" to="/" :image="heroNavigationAbout" text="Who we are and what we do"/>
+    <NavCard
+      header="Get A Quote"
+      to="/get-a-quote"
+      :image="heroNavigationGAQ"
+      text="Within 24 hours"
+    />
+    <NavCard header="Join Kokkos" to="/" :image="heroNavigationJoin" text="Remote web development"/>
+    <NavCard
+      header="Our Work"
+      to="/our-work"
+      :image="heroNavigationWork"
+      text="See previous projects"
+    />
+    <NavCard
+      header="Ka´Ching"
+      to="/"
+      :image="heroNavigationKaChing"
+      text="Our instant payout++ program"
+    />
+    <NavCard header="Chat with us" to="/" :image="heroNavigationChat" text="We are online"/>
     <Leaf/>
+    <Footer/>
   </section>
 </template>
 
 <script>
 import Leaf from "@/components/Leaf";
+import NavCard from "@/components/NavCard";
+import Footer from "@/components/Footer";
+
+import heroNavigationAbout from "@/assets/heroNavigationAbout.svg";
+import heroNavigationGAQ from "@/assets/heroNavigationGAQ.svg";
+import heroNavigationJoin from "@/assets/heroNavigationJoin.svg";
+import heroNavigationWork from "@/assets/heroNavigationWork.svg";
+import heroNavigationKaChing from "@/assets/heroNavigationKaChing.svg";
+import heroNavigationChat from "@/assets/heroNavigationChat.svg";
+
 export default {
-  components: { Leaf }
+  components: { Leaf, NavCard, Footer },
+  computed: {
+    heroNavigationAbout() {
+      return heroNavigationAbout;
+    },
+    heroNavigationGAQ() {
+      return heroNavigationGAQ;
+    },
+    heroNavigationJoin() {
+      return heroNavigationJoin;
+    },
+    heroNavigationWork() {
+      return heroNavigationWork;
+    },
+    heroNavigationKaChing() {
+      return heroNavigationKaChing;
+    },
+    heroNavigationChat() {
+      return heroNavigationChat;
+    }
+  }
 };
 </script>
 
 <style lang="scss">
+.contact-us {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  padding-bottom: 9rem;
+}
+
+.nav {
+  margin-bottom: 1rem;
+}
 </style>
---
+++ src/components/NavCard.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="nav-card">
+    <router-link :to="to" class="nav-card__content">
+      <h3 class="nav-card__header">{{header}}</h3>
+      <img class="illustration" :src="image">
+      <p class="nav-card__text">{{text}}</p>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["header", "image", "text", "to"]
+};
+</script>
+
+<style lang="scss">
+$nav-card-height: 260px;
+
+.nav-card,
+.nav-card__content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  text-decoration: none;
+}
+.nav-card {
+  max-width: 330px;
+  width: 100%;
+  height: 260px;
+  border: 1px solid #656d82;
+  border-radius: 4px;
+  margin: 15px;
+  cursor: pointer;
+  box-sizing: border-box;
+
+  &__content {
+    height: calc(260px - 10px);
+  }
+
+  &:hover {
+    border: 5px solid $color-primary;
+  }
+
+  &__header {
+    font-size: $font-size-navcard;
+    font-weight: 500;
+  }
+
+  .illustration {
+    max-width: 100px;
+    width: 100%;
+  }
+
+  &__text {
+    font-size: $font-size-b;
+    width: 230px;
+    font-weight: 300;
+    text-align: center;
+  }
+}
+</style>
