a68581fc1f9c9a28a20f059ba7524ee6a833e07e
turbomartine <petlundbreiby@gmail.com>
2019-05-14 09:21:09 +0200

    make navbar and about page

---
+++ src/components/ContactUs.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="about-page">
+    <div class="about">
+      <Header title="About" description="About us" text="Who we are and what we do"/>
+      <img :src="hero">
+
+      <Leaf/>
+      <Footer/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header";
+import Leaf from "@/components/Leaf";
+import Footer from "@/components/Footer";
+
+import hero from "@/assets/hero.svg";
+
+export default {
+  components: { Header, Leaf, Footer },
+  computed: {
+    hero() {
+      return hero;
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.about-page {
+  display: flex;
+  justify-content: center;
+}
+.about {
+  display: flex;
+  flex-direction: column;
+  max-width: $size-nav-bar;
+  width: 100%;
+}
+</style>
